492d2834f0c94bf6d4ec996ecc7941e59e7dbc11
ebenhaj2005 <145969715+ebenhaj2005@users.noreply.github.com>
2024-12-11 15:48:00 +0100

    Update signUp.js

--- app/signUp.js
+++ app/signUp.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import { Link } from 'expo-router';
-
+import { useRouter } from 'expo-router';
 export default function signUp() {
   const [firstname, setFirstName] = useState('');
   const [lastname, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatpassword, setRepeatPassword] = useState('');
-
+  const router = useRouter();
 
   const handleSignUp = () => {
     if (firstname.trim().length === 0) {
@@ -31,8 +31,16 @@ export default function signUp() {
         Alert.alert('Error', 'Password is not matching');
         return;
       }
-    Alert.alert('Success', `Welcome, ${firstname } ${lastname}!`);
+    Alert.alert('Success', `Welcome, ${firstname } ${lastname}!`, [
+      {
+        text: 'OK',
+        onPress: () => router.push('/'), 
+      },
+    ]);
   };
+
+    
+  ;
 
   return (
     <View style={styles.container}>
@@ -82,8 +90,8 @@ export default function signUp() {
         Already have an account? Go to Login
       </Link>
     </View>
-  );
-}
+  )};
+
 
 const styles = StyleSheet.create({
   container: {
